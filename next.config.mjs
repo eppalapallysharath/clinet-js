1d985120e3d696bc6b8e0631a35155048d618e42
eppalapallysharath <106862981+eppalapallysharath@users.noreply.github.com>
2024-09-27 13:01:30 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ next.config.mjs
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+        domains: ['images.pexels.com'],
+      },
+      cluster: true,
+};
+
+export default nextConfig;
---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import Head from 'next/head'
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+      <h1>Head</h1>
+    </div>
+  )
+}
+
+export default Header
